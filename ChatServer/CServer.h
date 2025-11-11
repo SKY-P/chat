3c08f64a86e33926be71ea68488ca3dde2bceebd
yjh <3072129368@qq.com>
2025-11-11 12:52:20 +0800

    ChatServer

---
+++ ChatServer/CServer.h
@@ -0,0 +1,25 @@
+#pragma once
+#include<boost/asio.hpp>
+#include<memory>
+#include<map>
+#include<mutex>
+#include "CSession.h"
+
+using boost::asio::ip::tcp;
+
+class CServer
+{
+public:
+	CServer(boost::asio::io_context& io_context, short port);
+	~CServer();
+	void ClearSession(std::string);
+private:
+	void HandleAccept(std::shared_ptr<CSession>, const boost::system::error_code& error);
+	void StartAccept();
+	boost::asio::io_context& _io_context;
+	short _port;
+	tcp::acceptor _acceptor;
+	std::map<std::string, std::shared_ptr<CSession>> _sessions;
+	std::mutex _mutex;
+};
+
